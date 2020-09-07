8d9e91ac206465e1a2c1fc5d4399aa877ab39601
olivier G-R <olivier@fractalwire.io>
2020-09-07 07:39:19 +0200

    Remove ais folder for submodule replacement

--- ais/aisreceiver/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AisreceiverConfig(AppConfig):
-    name = 'aisreceiver'
--- ais/geoserver/app_settings.py
+++
@@ -1,2 +0,0 @@
-# Update interval to update geoserver's specific table in DB
-GEOSERVER_WINDOW = 30*60  # in seconds
